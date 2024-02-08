352d9bc7148df3b489ad4490c1932a3051788094
Sounak Dey <108277454+Dragoon4002@users.noreply.github.com>
2024-02-08 12:42:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const answers=[];
+
 const question=["What is the exact location of my train?","From which platform will my train depart?","At what time will my train arrive?","By how much time my train is delayed?"];
 function scripting(n){
     const view_box=document.getElementById("view_port");
@@ -19,4 +20,36 @@ function appear(){
             window.setTimeout(console.log('Works'), 2000);
         }
     }
+}
+
+function initMap() {
+    // Replace the coordinates with your desired location
+    const myLatLng = { lat: -34.397, lng: 150.644 };
+  
+    // Create a new map centered at the specified coordinates
+    const map = new google.maps.Map(document.getElementById('map-container'), {
+      zoom: 8, // Adjust the zoom level
+      center: myLatLng,
+    });
+  
+    // Add a marker at the specified coordinates
+    const marker = new google.maps.Marker({
+      position: myLatLng,
+      map,
+      title: 'Your Marker Title',
+    });
+}
+
+function new_location() {
+    const map = document.getElementById("map");
+    var locationInput = document.getElementById("locationsearchform").value;
+    
+    // Split the location input into an array
+    var location = locationInput.split(" ");
+
+    // Join the array elements with "+" and encode the resulting string
+    var loc = encodeURIComponent(location.join("+"));
+
+    // Replace the previous code with the updated URL
+    map.src = "https://www.google.co.in/maps?q=" + loc + "/@22.6779287,88.3434035,12.21z?entry=ttu&t=&z=13&ie=UTF8&iwloc=&output=embed";
 }
